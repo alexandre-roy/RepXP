eab1fad1558c9e6e05bace00d8bd76b99e699cb7
Anthony Pageau <antopageau@hotmail.com>
2025-08-28 19:21:26 -0400

    [Deploy] Modifier les hosts possibles

--- repxp/repxp/settings.py
+++ repxp/repxp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wuzq0$ivqo!l)gw=a%hh)x_3ns!h*3*_qxgdh=1g@gai*4mrqp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['repxp.ca']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
