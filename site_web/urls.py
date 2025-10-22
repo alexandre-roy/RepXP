33bc147e6b9038af27294306fcde0e1108b52c3e
Alexandre Roy <alexandre15roy@gmail.com>
2025-10-22 10:11:36 -0400

    [FEAT] page des entrainements

--- site_web/urls.py
+++ site_web/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('exercices/proposer', views.proposer_exercice, name='proposer_exercice'),
     path('workouts/new', views.new_workout, name='new_workout'),
     path('workouts/', views.my_workouts, name='my_workouts'),
+    path('workouts/delete/<int:workout_id>/', views.delete_workout, name='delete_workout'),
     path('signup/', views.register, name='signup'),
     path('accounts/signin', views.connexion, name='signin'),
     path('accounts/', include('django.contrib.auth.urls')),
